a7706198dd50c2126a780b7654516923a4562fc1
suyash0103 <suyashghuge@gmail.com>
2018-03-23 22:45:54 +0530

    points changed

--- lighthouse.c
+++ lighthouse.c
@@ -2,7 +2,7 @@
 #include <gl/glut.h>
 #include <stdio.h>
 
-int x1, y1, x2, y2;
+int x1, y1, x2, y2, x3, y3, x4, y4;
 
 void myInit()
 {
@@ -17,6 +17,53 @@ void draw_pixel(int x, int y)
 	glBegin(GL_POINTS);
 	glVertex2i(x, y);
 	glEnd();
+}
+
+void edgedetect(GLfloat x1,GLfloat y1,GLfloat x2,GLfloat y2,int *le,int *re)
+{
+            float mx,x,temp;
+            int i;
+            if((y2-y1)<0)    // if second point is below first point interchange them
+            {
+              temp=x1;x1=x2;x2=temp;
+              temp=y1;y1=y2;y2=temp;
+            }
+              if((y2-y1)!=0)      // if denominator is zero we can't find slope
+                        mx=(x2-x1)/(y2-y1);
+            else
+                        mx=x2-x1;    // y2-y1=0 implies line is horizontal
+            x=x1;
+            for(i=y1;i<y2;i++)        // starting from x1,y1 add slope mx to x
+            {                                  // and round it to find the next point on the
+                                                // line. For that particular scan line i
+                        if(x<le[i])         // insert the x value into either le or re.
+                                    le[i]=x; // Initially both le and re will contain same
+                                                // value...
+                        if(x>re[i])         // in the next time for the other edge
+                                    re[i]=x; // either le or re will change
+                        x+=mx;            // NOTE: le and re are integer arrays and x
+            }                                  // is float so automatic type conversion.
+}
+
+void scanfill(float x1,float y1,float x2,float y2,float x3,float y3,float x4,float y4)
+{
+            int le[500],re[500];
+            int i,y;
+            for(i=0;i<500;i++)   // initialize le and re array values
+            {
+                        le[i]=500;
+                        re[i]=0;
+            }
+            edgedetect(x1,y1,x2,y2,le,re);    // call edge detect four times
+            edgedetect(x2,y2,x3,y3,le,re);    // once for each edge.
+            edgedetect(x3,y3,x4,y4,le,re);
+            edgedetect(x4,y4,x1,y1,le,re);
+for(y=0;y<500;y++)        // for every scan line with value y
+{
+           if(le[y]<=re[y])            // refer to le and re arrays to see if a part
+                        for(i=le[y]+1;i<re[y];i++) // of the scanline is inside polygon
+                                    draw_pixel(i,y);       // if so draw a horizontal line from
+}                                                              // left edge to right edge
 }
 
 void draw_line(int x1, int x2, int y1, int y2)
@@ -75,55 +122,25 @@ void draw_line(int x1, int x2, int y1, int y2)
 	}
 }
 
-void horizon()
-{
-    /*glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-    glBegin(GL_QUADS);
-        glColor3d(94, 94, 94);
-        glVertex2f(-250, 250);
-        glColor3d(94, 94, 94);
-        glVertex2f(250, 250);
-        glColor3d(94, 94, 94);
-        glVertex2f(-250, 210);
-        glColor3d(94, 94, 94);
-        glVertex2f(250, 200);
-    glEnd();
-    glutSwapBuffers();*/
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-    glBegin(GL_QUADS);
-        glColor3d(94,94,94);
-        glVertex2d(-100,-100);
-        glColor3d(94,94,94);
-        glVertex2d(-100,100);
-        glColor3d(94,94,94);
-        glVertex2d(100,-100);
-        glColor3d(94,94,94);
-        glVertex2d(100,100);
-    glEnd();
-    glutSwapBuffers();
-}
-
 void myDisplay()
 {
-	draw_line(-260, 250, 250, 250);
-	draw_line(-260, -250, 250, 210);
-	draw_line(-260, 250, 210, 200);
-	draw_line(250, 250, 250, 210);
-	//horizon();
+	draw_line(x1, x2, y1, y2);
+	draw_line(x1, x3, y1, y3);
+	draw_line(x3, x4, y3, y4);
+	draw_line(x2, x4, y2, y4);
 	glFlush();
 }
 
 void main(int argc, char **argv)
 {
-	//printf( "Enter (x1, y1, x2, y2)\n");
-	//scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
-
-    x1 = -250;
-    y1 = 210;
-    x2 = 250;
-    y2 = 200;
+	x1 = -260;
+	y1 = 250;
+	x2 = 250;
+	y2 = 250;
+	x3 = -260;
+	y3 = 210;
+	x4 = 250;
+	y4 = 200;
 
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
