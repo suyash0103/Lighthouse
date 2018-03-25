a6f2484a386e4a77b0691a1bbd761832aac3e905
suyash0103 <suyashghuge@gmail.com>
2018-03-25 15:36:24 +0530

    lighthouse base

--- lighthouse.c
+++ lighthouse.c
@@ -8,6 +8,7 @@ int beach_x1, beach_y1, beach_x2, beach_y2, beach_x3, beach_y3, beach_x4, beach_
 int green1_x1, green1_y1, green1_x2, green1_y2, green1_x3, green1_y3, green1_x4, green1_y4;
 int green2_x1, green2_y1, green2_x2, green2_y2, green2_x3, green2_y3, green2_x4, green2_y4;
 int road_x1, road_y1, road_x2, road_y2, road_x3, road_y3, road_x4, road_y4;
+int l1_x, l1_y, l2_x, l2_y, l3_x, l3_y, l4_x, l4_y, l5_x, l5_y, l6_x, l6_y, l7_x, l7_y, l8_x, l8_y;
 
 void myInit()
 {
@@ -73,7 +74,7 @@ void scanfill(float x1, float y1, float x2, float y2, float x3, float y3, float 
     edgedetect(x2,y2,x3,y3,le,re);
     edgedetect(x3,y3,x4,y4,le,re);
     edgedetect(x4,y4,x1,y1,le,re);
-    for(y=0;y<500;y++)        // for every scan line with value y
+    for(y=0;y<500;y++)
     {
         if(le[y]<=re[y])
         {
@@ -150,6 +151,52 @@ void myDisplay()
     scanfill(green2_x1, green2_y1, green2_x2, green2_y2, green2_x3, green2_y3, green2_x4, green2_y4, 0.0, 0.49, 0.0);
     scanfill(road_x1, road_y1, road_x2, road_y2, road_x3, road_y3, road_x4, road_y4, 0.41, 0.41, 0.41);
 
+    // Block 1
+    scanfill(l1_x, l1_y, l2_x, l2_y, l3_x, l3_y, l4_x, l4_y, 1.0, 0.0, 0.0);
+    scanfill(l5_x, l5_y, l1_x + 1, l1_y, l4_x + 1, l4_y, l6_x, l6_y, 1.0, 0.0, 0.0);
+
+    // Block 2
+    l4_y = l1_y;
+    l3_y = l2_y;
+    l6_y = l5_y;
+    l1_y += 60;
+    l2_y += 60;
+    l5_y += 60;
+    scanfill(l1_x, l1_y, l2_x, l2_y, l3_x, l3_y, l4_x, l4_y, 1.0, 1.0, 1.0);
+    scanfill(l5_x, l5_y, l1_x + 1, l1_y, l4_x + 1, l4_y, l6_x, l6_y, 1.0, 1.0, 1.0);
+
+    // Block 3
+    l4_y = l1_y;
+    l3_y = l2_y;
+    l6_y = l5_y;
+    l1_y += 60;
+    l2_y += 60;
+    l5_y += 60;
+    scanfill(l1_x, l1_y, l2_x, l2_y, l3_x, l3_y, l4_x, l4_y, 1.0, 0.0, 0.0);
+    scanfill(l5_x, l5_y, l1_x + 1, l1_y, l4_x + 1, l4_y, l6_x, l6_y, 1.0, 0.0, 0.0);
+
+    // Block 4
+    l4_y = l1_y;
+    l3_y = l2_y;
+    l6_y = l5_y;
+    l1_y += 60;
+    l2_y += 60;
+    l5_y += 60;
+    scanfill(l1_x, l1_y, l2_x, l2_y, l3_x, l3_y, l4_x, l4_y, 1.0, 1.0, 1.0);
+    scanfill(l5_x, l5_y, l1_x + 1, l1_y, l4_x + 1, l4_y, l6_x, l6_y, 1.0, 1.0, 1.0);
+
+    // Block 5
+    l4_y = l1_y;
+    l3_y = l2_y;
+    l6_y = l5_y;
+    l1_y += 60;
+    l2_y += 60;
+    l5_y += 60;
+    scanfill(l1_x, l1_y, l2_x, l2_y, l3_x, l3_y, l4_x, l4_y, 1.0, 0.0, 0.0);
+    scanfill(l5_x, l5_y, l1_x + 1, l1_y, l4_x + 1, l4_y, l6_x, l6_y, 1.0, 0.0, 0.0);
+
+    draw_line(l1_x, l1_x, l1_y, 0, 0.41, 0.41, 0.41);
+
 	glFlush();
 }
 
@@ -210,11 +257,26 @@ void main(int argc, char **argv)
     green2_x4 = 320.5;
     green2_y4 = 0;
 
+    l1_x = 380;
+    l7_x = l1_x + 1;
+    l1_y = 10;
+    l2_x = 430 - 5;
+    l2_y = 20;
+    l3_x = 430 - 5;
+    l3_y = 0;
+    l4_x = 380;
+    l8_x = l4_x + 1;
+    l4_y = 0;
+    l5_x = 380 - 15;
+    l5_y = 10 + 15;
+    l6_x = 380 - 15;
+    l6_y = 0;
+
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE|GLUT_RGBA);
 	glutInitWindowSize(500, 500);
 	glutInitWindowPosition(0, 0);
-    glutCreateWindow("Bresenham's Line Drawing");
+    glutCreateWindow("Lighthouse");
     glutFullScreen();
     myInit();
 	glutDisplayFunc(myDisplay);
