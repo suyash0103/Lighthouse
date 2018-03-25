be6020ba7f31ca23d68bb1a95e984ba6455ebc1e
Dhvanil Hitulkumar Parikh <dhvanilhparikh@gmail.com>
2018-03-25 06:44:07 +0530

    color changes

--- lighthouse.c
+++ lighthouse.c
@@ -78,7 +78,6 @@ void scanfill(float x1, float y1, float x2, float y2, float x3, float y3, float 
             }
         }
     }
-    SwapBuffers();
 }
 
 void draw_line(int x1, int x2, int y1, int y2, float r, float g, float b)
@@ -139,23 +138,9 @@ void draw_line(int x1, int x2, int y1, int y2, float r, float g, float b)
 
 void myDisplay()
 {
-    // Drawing The Horizon
-	/*draw_line(horizon_x1, horizon_x2, horizon_y1, horizon_y2);
-	draw_line(horizon_x1, horizon_x3, horizon_y1, horizon_y3);
-	draw_line(horizon_x3, horizon_x4, horizon_y3, horizon_y4);
-	draw_line(horizon_x2, horizon_x4, horizon_y2, horizon_y4);*/
-	scanfill(horizon_x1, horizon_y1, horizon_x2, horizon_y2, horizon_x4, horizon_y4, horizon_x3, horizon_y3, 0.0, 99.0, 1);
-
-	// Drawing The Sea
-	/*draw_line(sea_x1, sea_x2, sea_y1, sea_y2);
-	draw_line(sea_x1, sea_x3, sea_y1, sea_y3);
-	draw_line(sea_x3, sea_x4, sea_y3, sea_y4);
-	draw_line(sea_x2, sea_x4, sea_y2, sea_y4);*/
-    //scanfill(-250, 0, -250, 100, 250, 100, 250, 0);
-    //scanfill(0, 0, 0, 150, 250, 250, 250, 0);
+	scanfill(horizon_x1, horizon_y1, horizon_x2, horizon_y2, horizon_x4, horizon_y4, horizon_x3, horizon_y3, 0.0, 99, 1);
     scanfill(sea_x1, sea_y1, sea_x2, sea_y2, sea_x3, sea_y3, sea_x4, sea_y4, 0.0, 0.0, 100.0);
-
-    scanfill(beach_x1, beach_y1, beach_x2, beach_y2, beach_x3, beach_y3, beach_x4, beach_y4, 76, 69, 50);
+    scanfill(beach_x1, beach_y1, beach_x2, beach_y2, beach_x3, beach_y3, beach_x4, beach_y4, .9, .33, .19);
 
 	glFlush();
 }
@@ -190,7 +175,7 @@ void main(int argc, char **argv)
     beach_y4 = 0;
 
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
+	glutInitDisplayMode(GLUT_SINGLE|GLUT_RGBA);
 	glutInitWindowSize(500, 500);
 	glutInitWindowPosition(0, 0);
     glutCreateWindow("Bresenham's Line Drawing");
