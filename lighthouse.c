6b1427314c9d598457f51cfc78e452d8e76397dc
suyash0103 <suyashghuge@gmail.com>
2018-03-25 18:25:20 +0530

    continuous colour

--- lighthouse.c
+++ lighthouse.c
@@ -9,13 +9,15 @@ int green1_x1, green1_y1, green1_x2, green1_y2, green1_x3, green1_y3, green1_x4,
 int green2_x1, green2_y1, green2_x2, green2_y2, green2_x3, green2_y3, green2_x4, green2_y4;
 int road_x1, road_y1, road_x2, road_y2, road_x3, road_y3, road_x4, road_y4;
 int l1_x, l1_y, l2_x, l2_y, l3_x, l3_y, l4_x, l4_y, l5_x, l5_y, l6_x, l6_y, l7_x, l7_y, l8_x, l8_y;
+float scale_x=2.736;
+float scale_y=1.44;
 
 void myInit()
 {
 	glClear(GL_COLOR_BUFFER_BIT);
 	glClearColor(0.0, 0.0, 0.0, 1.0);
 	glMatrixMode(GL_PROJECTION);
-	gluOrtho2D(0, 500,0, 500);
+	gluOrtho2D(0, 1368, 0, 720);
 }
 
 void draw_pixel(int x, int y, float r, float g, float b)
@@ -63,18 +65,19 @@ void edgedetect(GLfloat x1,GLfloat y1,GLfloat x2,GLfloat y2,int *le,int *re)
 
 void scanfill(float x1, float y1, float x2, float y2, float x3, float y3, float x4, float y4, float r, float g, float b)
 {
-    int le[500],re[500];
+    int le[1080],re[1080];
     int i, y;
-    for(i=0;i<500;i++)
+    for(i=0;i<1080;i++)
     {
         le[i]=1000;
         re[i]=0;
     }
+
     edgedetect(x1,y1,x2,y2,le,re);
     edgedetect(x2,y2,x3,y3,le,re);
     edgedetect(x3,y3,x4,y4,le,re);
     edgedetect(x4,y4,x1,y1,le,re);
-    for(y=0;y<500;y++)
+    for(y=0;y<1080;y++)
     {
         if(le[y]<=re[y])
         {
@@ -153,52 +156,55 @@ void myDisplay()
 
     // Block 1
     scanfill(l1_x, l1_y, l2_x, l2_y, l3_x, l3_y, l4_x, l4_y, 1.0, 0.0, 0.0);
-    scanfill(l5_x, l5_y, l1_x + 1, l1_y, l4_x + 1, l4_y, l6_x, l6_y, 1.0, 0.0, 0.0);
+    scanfill(l5_x, l5_y, l1_x, l1_y, l4_x, l4_y, l6_x, l6_y, 1.0, 0.0, 0.0);
 
     // Block 2
     l4_y = l1_y;
     l3_y = l2_y;
     l6_y = l5_y;
-    l1_y += 60;
-    l2_y += 60;
-    l5_y += 60;
+    l1_y += 80;
+    l2_y += 80;
+    l5_y += 80;
     scanfill(l1_x, l1_y, l2_x, l2_y, l3_x, l3_y, l4_x, l4_y, 1.0, 1.0, 1.0);
-    scanfill(l5_x, l5_y, l1_x + 1, l1_y, l4_x + 1, l4_y, l6_x, l6_y, 1.0, 1.0, 1.0);
+    scanfill(l5_x, l5_y, l1_x, l1_y, l4_x, l4_y, l6_x, l6_y, 1.0, 1.0, 1.0);
 
     // Block 3
     l4_y = l1_y;
     l3_y = l2_y;
     l6_y = l5_y;
-    l1_y += 60;
-    l2_y += 60;
-    l5_y += 60;
+    l1_y += 80;
+    l2_y += 80;
+    l5_y += 80;
     scanfill(l1_x, l1_y, l2_x, l2_y, l3_x, l3_y, l4_x, l4_y, 1.0, 0.0, 0.0);
-    scanfill(l5_x, l5_y, l1_x + 1, l1_y, l4_x + 1, l4_y, l6_x, l6_y, 1.0, 0.0, 0.0);
+    scanfill(l5_x, l5_y, l1_x, l1_y, l4_x, l4_y, l6_x, l6_y, 1.0, 0.0, 0.0);
 
     // Block 4
     l4_y = l1_y;
     l3_y = l2_y;
     l6_y = l5_y;
-    l1_y += 60;
-    l2_y += 60;
-    l5_y += 60;
+    l1_y += 80;
+    l2_y += 80;
+    l5_y += 80;
     scanfill(l1_x, l1_y, l2_x, l2_y, l3_x, l3_y, l4_x, l4_y, 1.0, 1.0, 1.0);
-    scanfill(l5_x, l5_y, l1_x + 1, l1_y, l4_x + 1, l4_y, l6_x, l6_y, 1.0, 1.0, 1.0);
+    scanfill(l5_x, l5_y, l1_x, l1_y, l4_x, l4_y, l6_x, l6_y, 1.0, 1.0, 1.0);
 
     // Block 5
     l4_y = l1_y;
     l3_y = l2_y;
     l6_y = l5_y;
-    l1_y += 60;
-    l2_y += 60;
-    l5_y += 60;
+    l1_y += 80;
+    l2_y += 80;
+    l5_y += 80;
     scanfill(l1_x, l1_y, l2_x, l2_y, l3_x, l3_y, l4_x, l4_y, 1.0, 0.0, 0.0);
-    scanfill(l5_x, l5_y, l1_x + 1, l1_y, l4_x + 1, l4_y, l6_x, l6_y, 1.0, 0.0, 0.0);
+    scanfill(l5_x, l5_y, l1_x, l1_y, l4_x, l4_y, l6_x, l6_y, 1.0, 0.0, 0.0);
 
     // Line for 3D Effect
     draw_line(l1_x, l1_x, l1_y, 0, 0.41, 0.41, 0.41);
 
-    //scanfill(l1_x - 10, l1_y, )
+    // Ceiling
+    //scanfill(l1_x - 1, l1_y + 1, l2_x + 1, l2_y - 1, l3_x + 1, l3_y - 1, l4_x - 1, l4_y - 1, 1.0, 1.0, 1.0);
+    //scanfill(l5_x - 1, l5_y + 1, l1_x + 1, l1_y - 1, l4_x + 1, l4_y - 1, l6_x - 1, l6_y - 1, 1.0, 1.0, 1.0);
+
 
 	glFlush();
 }
@@ -206,80 +212,77 @@ void myDisplay()
 void main(int argc, char **argv)
 {
 	horizon_x1 = 0;
-	horizon_y1 = 500;
-	horizon_x2 = 500;
-	horizon_y2 = 500;
+	horizon_y1 = 500*scale_y;
+	horizon_x2 = 500*scale_x;
+	horizon_y2 = 500*scale_y;
 	horizon_x3 = 0;
-	horizon_y3 = 440;
-	horizon_x4 = 500;
-	horizon_y4 = 435;
+	horizon_y3 = 440*scale_y;
+	horizon_x4 = 500*scale_x;
+	horizon_y4 = 435*scale_y;
 
     sea_x1 = 0;
     sea_y1 = 0;
     sea_x2 = 0;
-    sea_y2 = 440;
-    sea_x3 = 260;
-    sea_y3 = 437.5;
-    sea_x4 = 210;
+    sea_y2 = 440*scale_y;
+    sea_x3 = 260*scale_x;
+    sea_y3 = 437.5*scale_y;
+    sea_x4 = 210*scale_x;
     sea_y4 = 0;
 
-    beach_x1 = 259;
-    beach_y1 = 437.5;
-    beach_x2 = 272.5;
-    beach_y2 = 437.5;
-    beach_x3 = 260;
+    beach_x1 = 259*scale_x;
+    beach_y1 = 437.5*scale_y;
+    beach_x2 = 272.5*scale_x;
+    beach_y2 = 437.5*scale_y;
+    beach_x3 = 260*scale_x;
     beach_y3 = 0;
-    beach_x4 = 209;
+    beach_x4 = 209*scale_x;
     beach_y4 = 0;
 
-    green1_x1 = 271.5;
-    green1_y1 = 437.5;
-    green1_x2 = 335;
-    green1_y2 = 437.5;
-    green1_x3 = 322.5;
+    green1_x1 = 271.5*scale_x;
+    green1_y1 = 437.5*scale_y;
+    green1_x2 = 335*scale_x;
+    green1_y2 = 437.5*scale_y;
+    green1_x3 = 322.5*scale_x;
     green1_y3 = 0;
-    green1_x4 = 259;
+    green1_x4 = 259*scale_x;
     green1_y4 = 0;
 
 
-    road_x1 = 334;
-    road_y1 = 437.5;
-    road_x2 = 340;
-    road_y2 = 437.5;
-    road_x3 = 345;
+    road_x1 = 334*scale_x;
+    road_y1 = 437.5*scale_y;
+    road_x2 = 340*scale_x;
+    road_y2 = 437.5*scale_y;
+    road_x3 = 345*scale_x;
     road_y3 = 0;
-    road_x4 = 321.5;
+    road_x4 = 321.5*scale_x;
     road_y4 = 0;
 
-    green2_x1 = 339;
-    green2_y1 = 437.5;
-    green2_x2 = 510;
-    green2_y2 = 437.5;
-    green2_x3 = 510;
+    green2_x1 = 339*scale_x;
+    green2_y1 = 437.5*scale_y;
+    green2_x2 = 510*scale_x;
+    green2_y2 = 437.5*scale_y;
+    green2_x3 = 510*scale_x;
     green2_y3 = 0;
-    green2_x4 = 320.5;
+    green2_x4 = 320.5*scale_x;
     green2_y4 = 0;
 
-    l1_x = 380;
-    l7_x = l1_x + 1;
+    l1_x = 380 + 600;
     l1_y = 10;
-    l2_x = 430 - 5;
+    l2_x = (470 - 5) + 600;
     l2_y = 20;
-    l3_x = 430 - 5;
+    l3_x = (470 - 5) + 600;
     l3_y = 0;
-    l4_x = 380;
-    l8_x = l4_x + 1;
+    l4_x = 380 + 600;
     l4_y = 0;
-    l5_x = 380 - 15;
-    l5_y = 10 + 15;
-    l6_x = 380 - 15;
+    l5_x = (370 - 10) + 600;
+    l5_y = 25;
+    l6_x = (370 - 10) + 600;
     l6_y = 0;
-
 
 
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE|GLUT_RGBA);
-	glutInitWindowSize(500, 500);
+	glutInitWindowSize(1368, 720);
 	glutInitWindowPosition(0, 0);
     glutCreateWindow("Lighthouse");
     glutFullScreen();
